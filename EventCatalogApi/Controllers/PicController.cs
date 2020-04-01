bfaf47ce4ca228d3bd8c0ec773f910076fb99405
SaranyaShanmgm <saranmail21@gmail.com>
2020-04-01 14:09:47 -0700

    Included Pic Controller
    Included Pic Controller to Micoservice.

---
+++ EventCatalogApi/Controllers/PicController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventCatalogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PicController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+        public PicController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetImage(int id)
+        {
+           var WebRoot= _env.WebRootPath;
+           var path= Path.Combine($"{WebRoot}/Pics/", $"Event{id}.jpg");
+            var buffer = System.IO.File.ReadAllBytes(path);
+            return File(buffer, "image/jpeg");
+        }
+    }
+}
