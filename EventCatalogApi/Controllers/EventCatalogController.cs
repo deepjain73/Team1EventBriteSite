91f362314307b84200fa3f362f01b1988609ad26
Pooja Diwakar <poojadiwakar@gmail.com>
2020-04-02 17:51:59 -0700

    Created EventCatalogController and ViewModal. Added one api and one viewmodel

---
+++ EventCatalogApi/Controllers/EventCatalogController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventCatalogApi.Data;
+using EventCatalogApi.Domain;
+using EventCatalogApi.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EventCatalogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventCatalogController : ControllerBase
+    {
+        private readonly EventCatalogContext _context;
+        private readonly IConfiguration _config;
+        public EventCatalogController(EventCatalogContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> Items(
+            [FromQuery]int pageIndex = 0,
+            [FromQuery] int pageSize = 6)
+        {
+            var itemsCount = await _context.EventItems.LongCountAsync();
+            var items = await _context.EventItems
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            items = ChangePictureUrl(items);
+            var model = new PaginatedEventsViewModel<EventItem>
+            {
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                Count = itemsCount,
+                Data = items
+            };
+            return Ok(model);
+        }
+
+        private List<EventItem> ChangePictureUrl(List<EventItem> items)
+        {
+            items.ForEach(
+                e => e.PictureUrl =
+                e.PictureUrl.Replace("http://externalcatalogbaseurltobereplaced",
+                 _config["ExternalEventCatalogBaseUrl"])
+                );
+            return items;
+        }
+    }
+}
