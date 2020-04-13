e4bda0f5b63e27a713c55c97dc94af1552769e71
SaranyaShanmgm <saranmail21@gmail.com>
2020-04-12 19:23:26 -0700

    Included WebMvc Service & Models.
    Created WebMvc service and Models folder.

---
+++ WebMVC/Services/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMVC.Models;
+
+namespace WebMVC.Services
+{
+    public interface IEventService
+    {
+        Task<Event> GetEventItemsAsync(int page, int size);
+    }
+}
