5fa2ec7fcf4ee4106e6cbe42484032df5b01f80f
deepjain73 <38891452+deepjain73@users.noreply.github.com>
2020-04-14 18:54:18 -0700

    Added web part

---
+++ WebMVC/ViewModels/EventIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMVC.Models;
+
+namespace WebMVC.ViewModels
+{
+    public class EventIndexViewModel
+    {
+        public PaginationInfo PaginationInfo { get; set; }
+        public IEnumerable<SelectListItem> Category { get; set; }
+        public IEnumerable<SelectListItem> Type { get; set; }
+        public IEnumerable<EventItem> EventItems { get; set; }
+
+        //public int? CategoryFilterApplied { get; set; }
+        //public int? TypesFilterApplied { get; set; }
+
+    }
+}
