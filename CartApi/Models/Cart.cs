f49180135b60ca691c5ffc5701b92cf1af4cb80a
Pooja Diwakar <poojadiwakar@gmail.com>
2020-04-24 23:51:44 -0700

    Created Cart Api and completed Models

---
+++ CartApi/Models/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartApi.Models
+{
+    public class Cart
+    {
+        public string BuyerID { get; set; }
+        public List<CartItem> Items { get; set; }
+        public Cart(string cartId)
+        {
+            BuyerID = cartId;
+            Items = new List<CartItem>  ();
+        }
+    }
+}
